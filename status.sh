b11c15fac8d26419287aba45ec03f63cd47e2f79
TheMinester <44733031+TheMinester@users.noreply.github.com>
2023-03-20 19:54:42 +0100

    Add files via upload
    Script qui affiche si le service fourni en argument est présent et si le service est alors en cours ou arrêté

---
+++ status.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script qui affiche si le service fourni en argument est présent
+# et si le service est alors en cours ou arrêté
+
+# La commande suivante se base sur le fait que chaque commande retourne un status après son exécution
+# Si la commande n'échoue pas le message "Service $1 is present and running." est affiché dans le terminal
+# Si la commande échoue le message "Service $1 is not present." est affiché dans le terminal
+
+pgrep -x $1 >/dev/null && echo "Service $1 is present and running." || echo "Service $1 is not present."
