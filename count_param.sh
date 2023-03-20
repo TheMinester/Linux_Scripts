c90763163f8110169359cc44e1ae179e75d152d0
TheMinester <44733031+TheMinester@users.noreply.github.com>
2023-03-20 09:54:41 +0100

    Add files via upload
    Script qui renvoie le nombre de paramètres avec lesquels il a été appelé

---
+++ count_param.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# script qui renvoie le nombre de paramètres avec lesquels il a été appelé
+# $# est la variable préexistante contenant le nombre d'arguments
+
+echo "Nombre d'arguments d'appel =" $#
