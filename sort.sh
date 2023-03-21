ba8b3c394456e0547d4d5e321fd0454dad4f41a6
TheMinester <44733031+TheMinester@users.noreply.github.com>
2023-03-21 12:02:25 +0100

    Update sort.sh
    Updated the sort command to ignore upper case and lower case

--- sort.sh
+++ sort.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script qui affiche ses paramètres dans l'ordre alphabétique suivi d'un retour à la ligne
 
-sorted=$(printf "$@" | sort) # trier la liste des arguments "$@" et la stocker dans $sorted
+sorted=$(printf "$@" | sort -f) # trier la liste des arguments "$@" et la stocker dans $sorted
 
 echo $sorted # ecrit dans le terminla la valeur de $sorted
