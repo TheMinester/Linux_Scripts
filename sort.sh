737ca87ceab994bf10b5ce9cbf3a353b41abd972
TheMinester <44733031+TheMinester@users.noreply.github.com>
2023-03-22 15:48:24 +0100

    Update sort.sh

--- sort.sh
+++ sort.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-# Script qui affiche ses paramètres dans l'ordre alphabétique suivi d'un retour à la ligne
+# Script qui affiche ses arguments dans l'ordre alphabétique suivi d'un retour à la ligne
 
-sorted=$(printf "$@" | sort -f) # trier la liste des arguments "$@" et la stocker dans $sorted
+# Auteur: Duteil Julien
+# Créé le: 20/03/2023
 
-echo $sorted # ecrit dans le terminal la valeur de $sorted
+for arg in "$@" # pour chaque argument
+do
+    if [[ "${arg}" =~ [^a-zA-Z] ]] # test si l'argument ne contient pas autre chose que des lettres
+    then
+        echo "l'argument \"$arg\" ne contient pas que des lettres"
+        exit
+    else
+        :
+    fi
+done
+
+for arg in "$@" # pour chaque argument
+do
+    echo "$arg" # écrire l'argument actuellement traité
+done | sort -f # envoyer le résultat vers sort qui trie les arguments et par défaut écrit avec un saut de ligne. -f ignore la casse.
