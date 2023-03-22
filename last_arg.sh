6378113ec17bf18f3916722e7bac268c77157ba5
TheMinester <44733031+TheMinester@users.noreply.github.com>
2023-03-22 15:46:38 +0100

    Update last_arg.sh

--- last_arg.sh
+++ last_arg.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Script qui affiche le dernier argument avec lequel il est invoqué
 
+# Auteur: Duteil Julien
+# Créé le: 20/03/2023
+
 # $# est le nombre d'argument donnés à l'exécution du script
 # Il suffit d'afficher le numéro d'argument équivalent à ce nombre
 
