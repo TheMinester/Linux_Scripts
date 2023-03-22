431c4b0e1e6b2e618deb3a6c0631ccb3d3ad47f1
TheMinester <44733031+TheMinester@users.noreply.github.com>
2023-03-22 15:43:22 +0100

    Update count_param.sh

--- count_param.sh
+++ count_param.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # script qui renvoie le nombre de paramètres avec lesquels il a été appelé
+
+# Auteur: Duteil Julien
+# Créé le: 20/03/2023
+
 # $# est la variable préexistante contenant le nombre d'arguments
 
 echo "Nombre d'arguments d'appel =" $#
