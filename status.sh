39889bff0f63be5a1e3c869f32dfa39789e9e0cc
TheMinester <44733031+TheMinester@users.noreply.github.com>
2023-03-22 15:48:55 +0100

    Update status.sh

--- status.sh
+++ status.sh
@@ -2,6 +2,9 @@
 # Script qui affiche si le service fourni en argument est présent
 # et si le service est alors en cours ou arrêté
 
+# Auteur: Duteil Julien
+# Créé le: 20/03/2023
+
 # La commande suivante se base sur le fait que chaque commande retourne un status après son exécution
 # Si la commande n'échoue pas le message "Service $1 is present and running." est affiché dans le terminal
 # Si la commande échoue le message "Service $1 is not present." est affiché dans le terminal
